d182a39ed22343aea0cd72361b615251c4f81cd6
Larissa <larissameyer13@gmail.com>
2021-01-22 13:08:14 +0100

    finished the Colorchanging scheme, added the articles and cleared up the structure

--- js/app.js
+++ js/app.js
@@ -2,23 +2,80 @@
 $(document).ready(function(){
 
     /* Loading navigation and footer in all html documents */
-    $('#navigation').load('/elements/navigation.html');
+    $('#navigation').load('/elements/navigation.html', function() {
+        setActiveNavItem();
+    });
     $('#footer').load('/elements/footer.html');
 
-    /* Load JSON-encoded data for the article list */
+    /* Load JSON-encoded data for the articles */
     $.getJSON('articles.json', function(respons){
-        console.log(respons)
+        // console.log(respons)
         $.each(respons, function(i, article){
-            console.log(article.title)
+            // console.log(article.title)
             
             /* assembles the html structure with the JSON strings for the article list (with the class article-list) */
-            $('.article-list').append('<li><a href="' + article.url + '"><img class="w-100" src="' + article.img + '">' + article.title + '</a></li>')
+            $('.article-list').append(`
+            <li>
+                <a href="${article.url}">
+                    <img class="w-100" src="${article.img}">
+                    ${article.title}
+                </a>
+            </li>
+            `);
 
             /* assembles the html structure with the JSON strings for the overview with all the articles (with the class all-articles) */
-            $('.all-articles').append('<li><a href="' + article.url + '"><img class="w-50" src="' + article.img + '">' + article.title + '</a></li>')
+            $('.all-articles').append(`
+            <div class="col mb-5">
+                <div class="card">
+                    <a href="${article.url}">
+                        <img src="${article.img}" class="card-img-top">
+                            <div class="card-body">
+                                <h5 class="card-title">${article.title}</h5>
+                                <p class="card-text">${article.teaser}</p>
+                                <p class="text-right mb-0"><small>${article.date}</small></p>
+                            </div>
+                    </a>
+                </div>
+            </div>
+            `);
+
+            /* Gets the single articles by URL for the artcile details and assembles the html structure with the JSON strings */
+            let articleId = parseInt(getArticleIdByUrl());
+
+            if (article.id === articleId) {
+                console.log(article.title);
+                $('#article-details').append(`
+                    <p class="text-right"><small>${article.date}</small></p>
+                    <img src="${article.img}" class="w-100">
+                    <h2>${article.title}</h2>
+                    <p>${article.pharagraph1}</p>
+                    <p>${article.pharagraph2}</p>
+                    <p>${article.pharagraph3}</p>
+                    <p class="text-right"><small>${article.author}</small></p>
+                `)
+            }
 
         })
     });
+
+    /** Forms the Artcile ID from a string to a number for the if above */
+    function getArticleIdByUrl() {
+        const urlParams = new URLSearchParams(window.location.search);
+        const articleId = urlParams.get('id');
+       
+        return articleId;
+    }
+
+    /* adds the active class to the URL from where on the page the viewer is right now */
+    function setActiveNavItem() {
+        var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+
+        $('.nav-link').each(function() {
+            if (this.href === path) {
+             $(this).addClass('active');
+            }
+        });
+    }
 
 
 });
--- js/theme-switcher.js
+++ js/theme-switcher.js
@@ -1,20 +1,61 @@
-$(document).ready(function(){
+/** Searches if a CSS Theme is saved in the Local Storage and if not, it saves the active CSS stylesheet in the LocalStorage. If there is no LocalStorage, it returns an error
+ * @param theme */
+function saveTheme(theme) {
+    if ('localStorage' in window && window['localStorage'] !== null) {
+        try {
+            localStorage.setItem('theme', theme);
+        } catch (error) {
+            console.log(error);
+            if (error == QUOTA_EXCEEDED_ERR) {
+                alert('Quota exceeded!');
+            }
+        }
+    } else {
+        alert('Cannot store user preferences as your browser do not support local storage');
+    }
+}
 
-    /* loads a given attribute (href) and the given Link to the html tag with the id "switch".  */
-    $('#dark-theme').click(function(){
-        $('#switch').attr('href', '/style/stylesheet-dark-mode.css');
-        // $('#dark-theme').attr('disabled', 'disabled');
-        // $('#light-theme').removeAttr('disabled', 'disabled');
-        // $('#pink-theme').removeAttr('disabled', 'disabled');
-    });
+/** Changes the CSS in the header. It makes a new Link with the CSS Stylesheet that was choosen
+ * @param theme */
+function changeCSS(theme) {
+    var oldlink = document.getElementById("theme");
 
-    $('#pink-theme').click(function(){
-        $('#switch').attr('href', '/style/stylesheet-pink-mode.css');
-    });
+    var newlink = document.createElement("link");
+    newlink.setAttribute("rel", "stylesheet");
+    newlink.setAttribute("id", "theme");
+    newlink.setAttribute("type", "text/css");
+    saveTheme(theme);
 
-    $('#light-theme').click(function(){
-        $('#switch').attr('href', '/style/stylesheet-light-mode.css');
+    theme = 'style/stylesheet-' + theme + '-mode.css';
+    newlink.setAttribute("href", theme);
 
-    });
+    oldlink.replaceWith(newlink);
+}
 
-});
+
+/** Loads the default theme or the last choosen CSS version from the localStorage */
+window.onload = function() {
+    // load set item from LocalStorage if it's possible. If there is no Local Storage, it gives out an error
+    if ('localStorage' in window && window['localStorage'] !== null) {
+        try {
+            var theme = localStorage.getItem('theme');
+             // if no theme is/was choosen, set default theme as CSS
+            if(theme === null) {
+                theme = 'light';
+            }
+
+            //sets the CSS in the header
+            changeCSS(theme);
+
+        } catch (error) {
+            console.log(error);
+            if (error == QUOTA_EXCEEDED_ERR) {
+                alert('Quota exceeded!');
+            }
+        }
+    } else {
+        alert('Cannot store user preferences as your browser do not support local storage');
+    }
+
+   
+}
